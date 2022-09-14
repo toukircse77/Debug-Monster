08ca221095019ec9420e0d424be2381d41e547be
toukircse <toukirahmed610@gmail.com>
2022-09-14 22:51:07 +0600

    debug time spent

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = Math.floor(finishTime - startTime) / 1000;
 
   // show result modal
   resultModal.innerHTML = "";
@@ -136,7 +136,7 @@ startBtn.addEventListener("click",function(){
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpent = Math.floor((currentTime - startTime) / 1000);
 
 
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
